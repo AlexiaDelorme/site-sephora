ec9f6ea6bf7b1ca6e6ef1df48c711325872df5de
Alexia Delorme <46656955+AlexiaDelorme@users.noreply.github.com>
2020-11-11 17:25:41 +0100

    Added path to env.py file

--- main/settings.py
+++ main/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
+from os import path
+if path.exists('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    os.environ.get('HOSTNAME'),
+    '127.0.0.1',
+]
 
 # Application definition
 
