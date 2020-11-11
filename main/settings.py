f9e7d96e3861e9baa15de8cca96565aba29e58b2
Alexia Delorme <46656955+AlexiaDelorme@users.noreply.github.com>
2020-11-11 17:09:26 +0100

    Removed secret key from settings.py and updated .gitignore file

--- main/settings.py
+++ main/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oat%^7_g*%(_&u%yh0&a(va8t(d8p@or7pns))54r!)s_4w7m&'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
