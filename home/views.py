7468b23049973187c70c917b580c37cd9ea98af1
Alexia Delorme <46656955+AlexiaDelorme@users.noreply.github.com>
2020-11-13 18:45:02 +0100

    Created view and template for specialty detail page

--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.conf import settings
 from .forms import ContactForm
 from .models import Specialty, ListForSpecialty
@@ -20,3 +20,21 @@ def home_view(request):
         # "emailjs_user": # emailjs_user
     }
     return render(request, "home.html", context)
+
+
+def specialty_detail_view(request, pk):
+    """
+    View to return detail about each specialty
+    """
+
+    # Return the list of all sub-specialty matching the specialty id
+    specialty = get_object_or_404(Specialty, pk=pk)
+    sub_specialties = ListForSpecialty.objects.all().filter(related_specialty=specialty)
+
+    context = {
+        "page_title": specialty,
+        "specialty": specialty,
+        "sub_specialties": sub_specialties
+    }
+
+    return render(request, "specialty_detail.html", context)
