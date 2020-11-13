fcdcd70c38b94c1bde54da463c190c919146a3d8
Alexia Delorme <46656955+AlexiaDelorme@users.noreply.github.com>
2020-11-13 17:26:52 +0100

    Added specialty to home page context

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.conf import settings
 from .forms import ContactForm
+from .models import Specialty, ListForSpecialty
 
 
 def home_view(request):
@@ -10,10 +11,12 @@ def home_view(request):
     form = ContactForm()
 
     # emailjs_user = settings.EMAILJS_USER
+    specialties = Specialty.objects.all()
 
     context = {
         "page_title": "Home",
         "form": form,
+        "specialties": specialties
         # "emailjs_user": # emailjs_user
     }
     return render(request, "home.html", context)
