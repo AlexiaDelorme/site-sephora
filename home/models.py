b640019cae525d00826d6ab234a5d05cc0275ae1
Alexia Delorme <46656955+AlexiaDelorme@users.noreply.github.com>
2020-11-13 16:57:05 +0100

    Amended max_length char for icon field

--- home/models.py
+++ home/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Specialty(models.Model):
     name = models.CharField(max_length=100)
-    icon = models.CharField(max_length=50)
+    icon = models.CharField(max_length=150)
 
 
 class ListForSpecialty(models.Model):
