f725dc06583cd527d9ae2224b8fb6e305fe9e908
Alexia Delorme <46656955+AlexiaDelorme@users.noreply.github.com>
2020-11-29 16:19:43 +0100

    Display model name for admin panel

--- home/models.py
+++ home/models.py
@@ -5,12 +5,22 @@ class Specialty(models.Model):
     name = models.CharField(max_length=100)
     icon = models.CharField(max_length=150)
 
+    def __str__(self):
+        return self.name
+
 
 class SubSpecialty(models.Model):
     name = models.CharField(max_length=100)
     related_specialty = models.ForeignKey(Specialty, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
 
 class ListForSubSpecialty(models.Model):
     name = models.CharField(max_length=100)
-    related_sub_specialty = models.ForeignKey(SubSpecialty, on_delete=models.CASCADE)
+    related_sub_specialty = models.ForeignKey(
+        SubSpecialty, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
